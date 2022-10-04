4cb02168b6aa40ff1d828998c08badae0b2e23d0
Kistin-git <kistinia@gmail.com>
2022-10-04 17:54:16 +0300

    commit_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,34 +3,28 @@ import java.util.Objects;
 public class Main {
 
     public static void main(String[] args) {
+
         Computer[] Computer = new Computer[6];
+
         Notebook notebook = new Notebook();
         Server server = new Server();
         Desktop desktop = new Desktop();
         Netbook netbook = new Netbook();
         Monoblock monoblock = new Monoblock();
         Nettop nettop = new Nettop();
-        Computer[1] = notebook;
-        Computer[2] = netbook;
-        Computer[3] = nettop;
-        Computer[4] = monoblock;
-        Computer[5] = server;
-        Computer[0] = desktop;
+
+        Computer[0] = notebook;
+        Computer[1] = netbook;
+        Computer[2] = nettop;
+        Computer[3] = monoblock;
+        Computer[4] = server;
+        Computer[5] = desktop;
+
         for (int i = 0; i < 6; i++) {
             Computer[i].turnon();
             Computer[i].internetconnect();
             Computer[i].turnoff();
         }
-
-
-
-
-
-
-
-        notebook.turnon();
-        notebook.internetconnect();
-        notebook.turnoff();
 
 
     }
